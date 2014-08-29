c1899b17dc6fb0f3f2b2bfddacda741b018d3d02
Joseph Piacquadio <Joseph.Piacquadio@gmail.com>
2014-08-29 19:30:20 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name    "Joseph Piacquadio"
+    email    "joseph.piacquadio@gmail.com"
+    password  "foobar"
+    password_confirmation  "foobar"
+  end
+end
